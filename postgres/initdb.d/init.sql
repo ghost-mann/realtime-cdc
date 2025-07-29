e95d824ed2e040577ac534e94ea3d09564f35fd5
ghost-mann <iamaob8@protonmail.com>
2025-07-29 04:38:04 +0300

    feat: added orderbook table sql command in init.sql

--- postgres/initdb.d/init.sql
+++ postgres/initdb.d/init.sql
@@ -17,6 +17,17 @@ CREATE TABLE public.latest_prices (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS public.order_book (
+    id SERIAL PRIMARY KEY,
+    symbol VARCHAR(20) NOT NULL,
+    last_update_id BIGINT NOT NULL,
+    side VARCHAR(4) NOT NULL, -- Stores 'bid' or 'ask'
+    price NUMERIC NOT NULL,
+    quantity NUMERIC NOT NULL,
+    -- It's good practice to record when the row was inserted into our database
+    ingested_at TIMESTAMPTZ DEFAULT NOW()
+);
+
 
 
 ALTER TABLE public.recent_trades REPLICA IDENTITY FULL; -- Recommended for Debezium to provide the "before" state of a row on UPDATE and DELETE events.
